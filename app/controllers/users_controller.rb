44c0c810d3a7f0e152e34e463a0c28bb26f96a28
philliphue <phue@chicagobooth.edu>
2015-11-24 00:03:43 -0600

    Bones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = Photo.where(:user_id => @user.id)
+    # @comments = Comment.where(:photo_id => @photos.id)
   end
 
   def liked
